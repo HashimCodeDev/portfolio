754e857e060b8ef44133e1bee6122793dc34e779
Hashim <hashimmohammed212@gmail.com>
2025-03-12 08:50:55 +0530

    Text added and size of terminal corrected

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Matrix from "./components/Matrix";
 
 export default function App() {
 	return (
-		<>
+		<div className="container">
+			<h1 className="header">HASHIM.TERMINAL</h1>
 			<Matrix />
 			<TerminalPortfolio />
-		</>
+		</div>
 	);
 }
